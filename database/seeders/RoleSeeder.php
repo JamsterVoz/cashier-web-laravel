3adbd61097d03f3a110a663ce1b21b8c33cbd510
JamsterVoz <helmijamilibrahim58@gmail.com>
2024-04-19 03:50:22 -0700

    test

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -12,7 +12,7 @@ class RoleSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run()
+    public function run(): void
     {
         $data = [
             'admin', 'employee'
